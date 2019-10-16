d25f85c09039d8ad45410b65decfff36dab2171c
Noel-elon <noelnwaelugo@gmail.com>
2019-10-16 10:14:56 +0100

    Created the splashScreen activity

---
+++ app/src/main/java/com/example/aad_team_35_animation_challenge/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.aad_team_35_animation_challenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
