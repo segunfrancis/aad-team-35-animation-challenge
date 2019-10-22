96be7105cf82a5a28691e5d13275acf364a8e927
Noel-elon <noelnwaelugo@gmail.com>
2019-10-22 21:54:42 +0100

    splash anim

--- app/src/main/java/com/example/aad_team_35_animation_challenge/SplashScreenActivity.java
+++ app/src/main/java/com/example/aad_team_35_animation_challenge/SplashScreenActivity.java
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.aad_team_35_animation_challenge.auth.AuthActivity;
 import com.example.aad_team_35_animation_challenge.onboarding.OnBoardingActivity;
 
 
 public class SplashScreenActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        imageView = findViewById(R.id.splashImage);
+        startAnim();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -27,5 +33,12 @@ public class SplashScreenActivity extends AppCompatActivity {
                 finish();
             }
         }, 1500);
+    }
+
+    private void startAnim() {
+        Animation animation = AnimationUtils.loadAnimation(getBaseContext(), R.anim.onboard_anim);
+        imageView.startAnimation(animation);
+
+
     }
 }
